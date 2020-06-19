ec886ed36d5818015cb0c928442d7efb0b03bf8b
pedrk <pedro.kondrat.ismart@gmail.com>
2020-06-19 14:09:10 -0300

    TRansformações no painel

--- conexoes/conexao.php
+++ conexoes/conexao.php
@@ -1,8 +1,8 @@
 <?php
 // Informações sobre a conexão
 $HOST =  '127.0.0.1';
-$USUARIO = 'admin';
-$SENHA = 'admin';
+$USUARIO = 'root';
+$SENHA = 'root';
 $DB = 'Login';
 
 $conexao = mysqli_connect($HOST, $USUARIO, $SENHA, $DB) or die ('Não foi possível conectar');
--- login/painel.php
+++ login/painel.php
@@ -6,6 +6,11 @@ if(!isset($_SESSION)){
 // Incluindo a verificação de login
 include('verificaLogin.php');
 ?>
-
-<h2>Olá, <?php echo $_SESSION['nome']; ?></h2>
+<link href="../template/estilo.css" rel="stylesheet">
+    <div class="apresentacao" style="height:200px;
+                                    width: 100%;
+                                    background-color: #BD5D38;">
+        <a href="http://smcsistemas.prefeitura.sp.gov.br/indexsistemas/"><img src="../Imagens/Icone.png" style="width:50%; height: 80%;"/></a>
+        <h2 style="color: rgba(255,255,255,0.5)">Olá, <?php echo $_SESSION['nome']?></h2>                       
+    <div>
 <h2><a href="logout.php">Sair</a></h2>
